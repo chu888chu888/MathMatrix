5ea086d8ae14fae9e4a55a369e16188fef5f760e
Tairraos <xiaole.tao@gmail.com>
2012-12-14 23:22:10 +0800

    refactor

--- js/mm.loader.js
+++ js/mm.loader.js
@@ -1,7 +1,31 @@
 /**
- * Created with JetBrains WebStorm.
- * User: 小乐
- * Date: 12-12-14
- * Time: 下午10:52
- * To change this template use File | Settings | File Templates.
+ * @file mm.loader
+ * @author Tairraos (http://xiaole.happylive.org)
+ * @Date 2012-12-14
  */
+(function($) {
+    'use strict';
+    var moduleList = [
+        'core',
+        'config',
+        'Matrix'
+    ];
+
+    /**
+     * load script
+     * @param name
+     */
+    function loadScript(name) {
+        var dom = document.createElement('script');
+        dom.type = 'text/javascript';
+        dom.async = false;
+        dom.src = ('js/' + name + '.js');
+        document.body.appendChild(dom);
+    }
+
+    $.each(moduleList, function(index) {
+        loadScript('mm.' + moduleList[index]);
+    });
+
+    loadScript('game');
+}(jQuery));
